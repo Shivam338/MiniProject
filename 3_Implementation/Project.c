7dd8833a76e8cdf7bad51f4fca2694a8f5a8a0fc
Shivam Dogra <35564889+Shivam338@users.noreply.github.com>
2021-04-16 01:21:00 +0530

    Update Project.c

--- 3_Implementation/Project.c
+++ 3_Implementation/Project.c
@@ -1,4 +1,39 @@
-#include<stdio.h>
+#include"Calendar_Calc.h"
+
+int NumberofDays(int mm,int yy){
+	int dd;
+	if(mm==4||mm==6||mm==9||mm==11)
+	  {
+	  	return dd=30;
+	  }
+	  
+	else if(yy%4==0&&yy%100!=0||yy%400==0)
+	  {
+	  	if(mm==2)
+	  	 {
+	  	   return dd=29;	
+	  	 }
+	  }
+	else if(mm==1||mm==3||mm==5||mm==7||mm==8||mm==10||mm==12)
+	    return dd=31;
+	    
+	else
+	  {
+	  	if(mm==2)
+	  	 {
+	  	   return dd=28;	
+	  	 }
+	  }
+	  
+	 return 0;
+}
+
+long int LeapYears(int yy){
+	int leapYears=(yy-1)/4-(yy-1)/100+(yy-1)/400;
+	long int dp=(yy-1)*365l+leapYears;
+	printf("--->%ld",dp);
+	return dp;
+}
 
 int main()
 {
@@ -18,35 +53,13 @@ int main()
 		printf("\nEnter the month: ");
 		scanf("%d",&mm);
 	}
-	while(mm<1||mm>12);
+	while(mm<1||mm>12);	
+	  	  
+	dd= NumberofDays(mm,yy);
+ 
+	dp= LeapYears(yy);
 	
-	if(mm==4||mm==6||mm==9||mm==11)
-	  {
-	  	dd=30;
-	  }
-	  
-	  if(yy%4==0&&yy%100!=0||yy%400==0)
-	  {
-	  	if(mm==2)
-	  	 {
-	  	   dd=29;	
-	  	 }
-	  }
-	  else
-	  {
-	  	if(mm==2)
-	  	 {
-	  	   dd=28;	
-	  	 }
-	  }
-	  
-	  if(mm==1||mm==3||mm==5||mm==7||mm==8||mm==10||mm==12)
-	    dd=31;
-	  
-	  leapYears=(yy-1)/4-(yy-1)/100+(yy-1)/400;
-	  dp=(yy-1)*365l+leapYears;
-	  
-	   switch(mm)
+	switch(mm)
 	   {
 	   	case 12:dp+=30;
 		case 11:dp+=31;
@@ -62,7 +75,7 @@ int main()
 		case  1:dp+=1;   
 	   }
 	   
-	   printf("\nmon tue wed thu fri sat sun");
+	printf("\nMon Tue Wed Thu Fri Sat Sun");
 	    switch(dp%7)
 	    {
 	    	case 1: count=0;
