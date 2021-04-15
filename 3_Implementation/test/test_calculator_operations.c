ba99391e79dfd859b1e93909e1f79ced5f5d5bea
Shivam Dogra <35564889+Shivam338@users.noreply.github.com>
2021-04-16 01:18:57 +0530

    Update test_calculator_operations.c

--- 3_Implementation/test/test_calculator_operations.c
+++ 3_Implementation/test/test_calculator_operations.c
@@ -1,16 +1,11 @@
 #include "unity.h"
-#include <calculator_operations.h>
 
-/* Modify these two lines according to the project */
-#include <calculator_operations.h>
-#define PROJECT_NAME    "Calculator"
+#include "Calendar.h"
+#define PROJECT_NAME    "Calendar"
 
 /* Prototypes for all the test functions */
-void test_add(void);
-void test_add_testcase2(void);
-void test_subtract(void);
-void test_multiply(void);
-void test_divide(void);
+void test_NumberofDays(void);
+void test_LeapYears(void);
 
 /* Required by the unity test framework */
 void setUp(){}
@@ -24,45 +19,28 @@ int main()
   UNITY_BEGIN();
 
 /* Run Test functions */
-  RUN_TEST(test_add);
-  RUN_TEST(test_add_testcase2);
-  RUN_TEST(test_subtract);
-  RUN_TEST(test_multiply);
-  RUN_TEST(test_divide);
+  RUN_TEST(test_NumberofDays);
+  RUN_TEST(test_LeapYears);
 
   /* Close the Unity Test Framework */
   return UNITY_END();
 }
 
 /* Write all the test functions */ 
-void test_add(void) {
-  TEST_ASSERT_EQUAL(30, add(10, 20));
-  TEST_ASSERT_EQUAL(-10, add(10, -20));
-  TEST_ASSERT_EQUAL(-30, add(-10, -20));
-  TEST_ASSERT_EQUAL(10, add(-10, 20));
+void test_NumberofDays(void) {
+  TEST_ASSERT_EQUAL(31, NumberofDays(1, 1999));
+  TEST_ASSERT_EQUAL(29, NumberofDays(2, 2000));
+  TEST_ASSERT_EQUAL(31, NumberofDays(12, 2012));
+  TEST_ASSERT_EQUAL(28, NumberofDays(02, 2021));
+  TEST_ASSERT_EQUAL(29, NumberofDays(02, 2004));
+  TEST_ASSERT_EQUAL(31, NumberofDays(8, 2029));
+  TEST_ASSERT_EQUAL(30, NumberofDays(9, 2023));
 }
-void test_add_testcase2(void) {
- 
-  /* Dummy fail*/
-  // TEST_ASSERT_EQUAL(1500, add(750, 7500));
-}
-void test_subtract(void) {
-  TEST_ASSERT_EQUAL(-3, subtract(0, 3));
-  
-  /* Dummy fail*/
-  // TEST_ASSERT_EQUAL(1, subtract(1000, 900));
-}
-
-void test_multiply(void) {
-  TEST_ASSERT_EQUAL(0, multiply(1, 0));
-  
-  /* Dummy fail*/
-  // TEST_ASSERT_EQUAL(2, multiply(2, 5));
+void test_LeapYears(void) {
+  TEST_ASSERT_EQUAL(729389, LeapYears(1998));
+  TEST_ASSERT_EQUAL(729754, LeapYears(1999));
+  TEST_ASSERT_EQUAL(734502, LeapYears(2012));
+  TEST_ASSERT_EQUAL(737424, LeapYears(2020));
+  TEST_ASSERT_EQUAL(731580, LeapYears(2004));
 }
 
-void test_divide(void) {
-  TEST_ASSERT_EQUAL(0, divide(1, 0));
-  
-  /* Dummy fail*/
-  // TEST_ASSERT_EQUAL(3, divide(2, 2));
-}
